04945c550a8d6644ccb7e37221bf138c9a5a3a03
ajit kumar <ajitjava2@gmail.com>
2020-03-16 22:20:05 +0530

    committoabcbranch

--- src/main/java/com/ajit/dao/EmployeeRepository.java
+++ src/main/java/com/ajit/dao/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package com.ajit.dao;
 
+import com.ajit.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends CrudRepository<Employee,String> {
 }
