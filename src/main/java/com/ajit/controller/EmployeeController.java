901231691f4f969f1bdfc28ce0a020b73057db41
ajit kumar <ajitjava2@gmail.com>
2020-03-17 22:31:26 +0530

    addedemployyeandaddress

---
+++ src/main/java/com/ajit/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.ajit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class EmployeeController {
+
+    @GetMapping(value = "/test")
+    public String getValue() {
+        return "demo";
+    }
+}
